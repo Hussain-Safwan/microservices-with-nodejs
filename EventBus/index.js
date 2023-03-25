362cfc84150fa91fc0b0af4fc40c9799dcf1a752
Hussain-Safwan <safwan.du16@gmail.com>
2023-03-25 15:12:04 +0600

    [BE] event-bus cache added

--- EventBus/index.js
+++ EventBus/index.js
@@ -8,12 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
 app.post("/event", (req, res) => {
   const event = req.body;
-  console.log(event);
+  events.push(event);
   axios.post("http://localhost:4000/events", event).catch((e) => {});
   axios.post("http://localhost:4001/events", event).catch((e) => {});
   axios.post("http://localhost:4002/events", event).catch((e) => {});
+  axios.post("http://localhost:4003/events", event).catch((e) => {});
 
   res.status(200).send({ message: "OK" });
 });
